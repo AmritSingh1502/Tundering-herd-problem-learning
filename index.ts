1176555558ab31c11fdbe03639542dc26520129e
Amrit Kumar Singh <113255930+AmritSingh1502@users.noreply.github.com>
2025-08-02 20:04:25 +0530

    added the simlation of the processing of disputes

--- index.ts
+++ index.ts
@@ -30,11 +30,35 @@ app.post('/disputes',(req,res) => {
 
 
 // get all disputes
-
 app.get('/api/disputes', ( req ,res ) => {
     res.json(disputes);
 });
 
+// simulate the processing of disputes
+setInterval(() => {
+    if(sqsQueue.length > 0) {
+        const dispute = sqsQueue.shift();
 
+        activeJobs.push({
+            jobId : `job_${Date.now()}`,
+            disputeId: dispute.id,
+            status: 'processing',
+            started: Date.now(),
+        });
+
+        // Simulate processing time
+        setTimeout(() => {
+            dispute.status = 'resolved';
+            // remove from active jobs
+            activeJobs = activeJobs.filter(job => job.disputeId !== dispute.id);
+            console.log(`Dispute ${dispute.id} processed and resolved.`);
+        }, 3000);
+    }
+}, 1000); // check sds queue every second
+
+// get all active jobs
+app.get('/api/jobs', (req, res) => {
+    res.json(activeJobs);
+});
 
 app.listen(3000, () => console.log('Disputes API server runnning on port 3000'));
